f93954e50e8e291ed4ea2a3151afa51a826a22b4
xingkun.zhang <1475795322@qq.com>
2023-02-14 00:34:40 +0800

    feat: init project

---
+++ WebApplication/WebApplication/Startup.cs
@@ -0,0 +1,50 @@
+﻿namespace WebApplication;
+public class Startup
+{
+    public Startup(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+    
+    
+    private IConfiguration Configuration { get; }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        // services.AddDbContextFactory<WebApplicationContext>(
+        //     options => options.UseSqlServer(Configuration["DBConnectionString"]), ServiceLifetime.Scoped);
+        services.AddControllers();
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+        services.ConfigureApplicationInsights(Configuration);
+
+        services.AddHealthCheckServices(Configuration);
+        services.AddMiddlewares(Configuration);
+        services.AddHostedServices();
+        services.AddDomainServices();
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+        else
+        {
+            app.UseExceptionHandler("/Error");
+            app.UseHsts();
+        }
+
+        // app.UseHttpsRedirection();
+        app.UseStaticFiles();
+        app.UseRouting();
+        app.UseAuthentication();
+        app.UseAuthorization();
+        app.UseHealthCheck();
+
+        app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
+    }
+}
---
+++ WebApplication/WebApplication/StartupServicesExtension.cs
@@ -0,0 +1,83 @@
+﻿using System.Reflection;
+using System.Text;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+
+namespace WebApplication;
+
+public static class StartupServicesExtension
+{
+    public static void AddHealthCheckServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddHealthChecks();
+        }
+
+        public static void AddMiddlewares(this IServiceCollection services, IConfiguration configuration)
+        {
+            // services.AddSingleton(new ServiceBusClient(configuration["ServiceBusConnectionString"]));
+            // services.AddSingleton(sp =>
+            // {
+            //     var client = sp.GetService<ServiceBusClient>();
+            //     var processor = client.CreateProcessor(configuration["ServiceBusTopicName"], configuration["ServiceBusSubscriptionName"]);
+            //     return processor;
+            // });
+        }
+
+        public static void AddDomainServices(this IServiceCollection services)
+        {
+            // services.AddScoped<IntegrationTestService>();
+            // services.AddScoped<IActionAccount, ActionAccount>();
+            // services.AddScoped<IServiceBusHandler, AccountHandler>();
+            // services.AddScoped<IActionAccountConfiguration, ActionAccountConfiguration>();
+        }
+
+        public static void AddHostedServices(this IServiceCollection services)
+        {
+            // services.AddAutoMapper(typeof(Program).Assembly);
+            // services.AddHostedService<AccountBackgroundService>();
+        }
+
+        public static void ConfigureApplicationInsights(this IServiceCollection serviceCollection,
+            IConfiguration configuration)
+        {
+            // serviceCollection.AddApplicationInsightsTelemetry(
+            //     new ApplicationInsightsServiceOptions
+            //     {
+            //         ConnectionString = configuration["AzureAppInsightConnectionString"]
+            //     });
+            // serviceCollection.ConfigureTelemetryModule<DependencyTrackingTelemetryModule>((module, o) =>
+            // {
+            //     module.EnableSqlCommandTextInstrumentation =
+            //         configuration.GetValue<bool>("EnableTrackSqlFullQueryTextInApplicationInsights");
+            // });
+        }
+
+        public static IApplicationBuilder UseHealthCheck(this IApplicationBuilder builder)
+        {
+            var version = Assembly.GetEntryAssembly()!.GetCustomAttribute<AssemblyInformationalVersionAttribute>()
+                ?.InformationalVersion;
+            var serverName = Environment.MachineName;
+
+            builder.UseHealthChecks("/health", new HealthCheckOptions
+            {
+                ResponseWriter = async (context, report) =>
+                {
+                    var sb = new StringBuilder();
+                    sb.AppendLine("Name = Account");
+                    sb.AppendLine($"Application Version = {version}");
+                    sb.AppendLine($"Status = {report.Status}");
+                    sb.AppendLine($"Server Name = {serverName}");
+                    sb.AppendLine();
+
+                    sb.AppendLine("Dependencies:");
+                    foreach (var (key, value) in report.Entries)
+                    {
+                        sb.AppendLine($"{key} = {value.Status}");
+                    }
+
+                    await context.Response.WriteAsync(sb.ToString());
+                }
+            });
+
+            return builder;
+        }
+}
