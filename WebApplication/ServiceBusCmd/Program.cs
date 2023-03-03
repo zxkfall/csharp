a0f4df3ac4358b9e305ee94c337dbd64fb6835bd
xingkun.zhang <1475795322@qq.com>
2023-03-03 08:15:18 +0800

    feat: use azure service bus to receive message

--- WebApplication/ServiceBusCmd/Program.cs
+++ WebApplication/ServiceBusCmd/Program.cs
@@ -21,8 +21,12 @@ var clientOptions = new ServiceBusClientOptions()
 { 
     TransportType = ServiceBusTransportType.AmqpWebSockets
 };
-client = new ServiceBusClient("<NAMESPACE-CONNECTION-STRING>", clientOptions);
-sender = client.CreateSender("<QUEUE-NAME>");
+// client = new ServiceBusClient("<NAMESPACE-CONNECTION-STRING>", clientOptions);
+var connectionString = "<NAMESPACE-CONNECTION-STRING>";
+client = new ServiceBusClient(connectionString, clientOptions);
+// sender = client.CreateSender("<QUEUE-NAME>/<TOPIC-NAME>");
+var queueOrTopicName = "update-account";
+sender = client.CreateSender(queueOrTopicName);
 
 // create a batch 
 using ServiceBusMessageBatch messageBatch = await sender.CreateMessageBatchAsync();
@@ -40,7 +44,7 @@ for (int i = 1; i <= numOfMessages; i++)
 try
 {
     // Use the producer client to send the batch of messages to the Service Bus queue
-    await sender.SendMessagesAsync(messageBatch);
+    // await sender.SendMessagesAsync(messageBatch);
     Console.WriteLine($"A batch of {numOfMessages} messages has been published to the queue.");
 }
 finally
@@ -53,3 +57,69 @@ finally
 
 Console.WriteLine("Press any key to end the application");
 Console.ReadKey();
+
+
+
+// =========================================================================
+
+
+// ServiceBusClient client;
+
+// the processor that reads and processes messages from the subscription
+ServiceBusProcessor processor;
+
+// handle received messages
+async Task MessageHandler(ProcessMessageEventArgs args)
+{
+    // TODO: Replace the <TOPIC-SUBSCRIPTION-NAME> placeholder
+    string body = args.Message.Body.ToString();
+    Console.WriteLine($"Received: {body} from subscription: <TOPIC-SUBSCRIPTION-NAME>");
+
+    // complete the message. messages is deleted from the subscription. 
+    await args.CompleteMessageAsync(args.Message);
+}
+
+// handle any errors when receiving messages
+Task ErrorHandler(ProcessErrorEventArgs args)
+{
+    Console.WriteLine(args.Exception.ToString());
+    return Task.CompletedTask;
+}
+// The Service Bus client types are safe to cache and use as a singleton for the lifetime
+// of the application, which is best practice when messages are being published or read
+// regularly.
+//
+// Create the clients that we'll use for sending and processing messages.
+// TODO: Replace the <NAMESPACE-CONNECTION-STRING> placeholder
+client = new ServiceBusClient(
+    connectionString);
+// create a processor that we can use to process the messages
+// TODO: Replace the <TOPIC-NAME> and <SUBSCRIPTION-NAME> placeholders
+processor = client.CreateProcessor(queueOrTopicName, "s1", new ServiceBusProcessorOptions());
+
+try
+{
+    // add handler to process messages
+    processor.ProcessMessageAsync += MessageHandler;
+
+    // add handler to process any errors
+    processor.ProcessErrorAsync += ErrorHandler;
+
+    // start processing 
+    await processor.StartProcessingAsync();
+
+    Console.WriteLine("Wait for a minute and then press any key to end the processing");
+    Console.ReadKey();
+
+    // stop processing 
+    Console.WriteLine("\nStopping the receiver...");
+    await processor.StopProcessingAsync();
+    Console.WriteLine("Stopped receiving messages");
+}
+finally
+{
+    // Calling DisposeAsync on client types is required to ensure that network
+    // resources and other unmanaged objects are properly cleaned up.
+    await processor.DisposeAsync();
+    await client.DisposeAsync();
+}
